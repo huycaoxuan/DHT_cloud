9b9e88a8f0c3deb13dbb8844fc0e43da337581ad
Huy Cao <33714452+huycaoxuan@users.noreply.github.com>
2021-09-25 16:23:40 +0700

    Update secrets.h

--- ESP32/secrets.h
+++ ESP32/secrets.h
@@ -1,8 +1,8 @@
 // Use this file to store all of the private credentials 
 // and connection details
 
-#define SECRET_SSID "Chit Soc"		// replace MySSID with your WiFi network name
-#define SECRET_PASS "Haminh101"	// replace MyPassword with your WiFi password
+#define SECRET_SSID "MySSID"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "MyPassword"	// replace MyPassword with your WiFi password
 
-#define SECRET_CH_ID 744994		// replace 0000000 with your channel number
-#define SECRET_WRITE_APIKEY "RYS3R5YH9P27PRWN"   // replace XYZ with your channel write API Key
+#define SECRET_CH_ID 0000000		// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "XYZ"   // replace XYZ with your channel write API Key
