b872ba7b69cf5df6b659abfd951b06f05f36f5f3
Huy Cao <33714452+huycaoxuan@users.noreply.github.com>
2021-09-25 15:49:08 +0700

    Create secrets.h

---
+++ secrets.h
@@ -0,0 +1,7 @@
+// Use this file to store all of the private credentials 
+// and connection details     
+
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883                   // use 8883 for SSL
+#define AIO_USERNAME    "your user name"
+#define AIO_KEY         "aio_xxxxxxxxxxxxx" // Put Your adafruit ID here
